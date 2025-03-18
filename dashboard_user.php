ed522ee0e4136ab24fabf78071dce4bbae2f6828
farra-fawnia <farrafawnia01@gmail.com>
2025-03-18 08:45:39 +0700

    membuat tampilan utama user pada dashboard_user.php

---
+++ dashboard_user.php
@@ -0,0 +1,39 @@
+<?php
+include (".includes/header_user.php");
+$title = "Dashboard";
+?>
+<div class="container-xxl flex-grow-1 container-p-y">
+<div class="d-flex justify-content-between flex-column-reverse flex-lg-row align-items-center pt-12 pb-10">
+
+<!--Background burem-->
+<img src="assets/img/hotel/hotel.jpg" 
+alt="hero background" class="position-absolute top-0 start-50 translate-middle-x object-fit-cover w-100" data-speed="1"  style="filter: blur(8px); height: 85%;">
+
+</div>
+
+<h1 class="position-absolute top-50 start-50 translate-middle text-white z-3 fw-bold fs-1">HOOKING</h1>
+<p class="position-absolute top-50 start-50 translate-middle text-white z-1 fw-bold fs-5" style="padding-top: 6%;">hotel booking terkeren semuka bumi dengan harga gokil berkualitas</p>
+
+<!-- yang ada orang main komputer -->
+<section class="pricing-free-trial bg-label-primary rounded-pill" style="margin-top: 400px;">
+  <div class="container">
+    <div class="position-relative">
+      <div class="d-flex justify-content-between flex-column-reverse flex-lg-row align-items-center pt-12 pb-10">
+        <div class="text-center text-lg-start">
+          <h4 class="text-primary mb-2">Mencari Hotel dengan kamar dan kualitas yang keren? HOOKING IN HERE!!</h4>
+          <p class="text-body mb-6 mb-md-11">You will get full access to with all the features for 14 days.</p>
+          <a href="" class="btn btn-primary">Start 14-day free trial</a>
+        </div>
+        <!-- image -->
+        <div class="text-center">
+          <img src="https://demos.themeselection.com/sneat-bootstrap-html-laravel-admin-template/demo/assets/img/illustrations/lady-with-laptop-light.png" class="img-fluid me-lg-5 pe-lg-1 mb-3 mb-lg-0" alt="Api Key Image" width="202" data-app-light-img="illustrations/lady-with-laptop-light.png" data-app-dark-img="illustrations/lady-with-laptop-dark.png">
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+</div>
+
+<?php
+include (".includes/footer.php");
+?>

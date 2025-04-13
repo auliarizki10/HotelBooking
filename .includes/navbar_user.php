efb978f417b9691cc075e61b655ccdddce844628
farra-fawnia <farrafawnia01@gmail.com>
2025-04-13 19:52:28 +0700

    mengubah navbar user pada file navbar_user.php

--- .includes/navbar_user.php
+++ .includes/navbar_user.php
@@ -1,12 +1,34 @@
 <!-- Navbar -->
 <nav class="layout-navbar container-xxl navbar navbar-expand-xl
 navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
-  <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-    <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-      <i class="bx bx-menu bx-sm"></i>
-    </a>
-  </div>
+  
   <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
+  <nav class="navbar navbar-example navbar-expand-lg ">
+                  <div class="container-fluid p-0">
+                  <nav class="navbar bg-body-tertiary">
+                  <div class="container-fluid">
+                    <a class="navbar-brand fw-bold d-flex align-items-center fs-3" href="#">
+                    <img src="assets/img/favicon/favicon.ico" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">HOOKING</a>
+                  </div>
+                  </nav>
+                    <button
+                      class="navbar-toggler"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#navbar-ex-4"
+                    >
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbar-ex-4">
+                      <div class="navbar-nav me-auto fs-5">
+                        <a class="nav-item nav-link active" href="javascript:void(0)">Home</a>
+                        <a class="nav-item nav-link" href="javascript:void(0)">About</a>
+                        <a class="nav-item nav-link" href="javascript:void(0)">Contact</a>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
     <ul class="navbar-nav flex-row align-items-center ms-auto">
       <!-- User -->
       <li class="nav-item navbar-dropdown dropdown-user dropdown">

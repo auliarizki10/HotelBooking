9a14adbbdf2097a69ae686ca9ff6e64d2ff84f9f
farra-fawnia <farrafawnia01@gmail.com>
2025-05-04 15:55:01 +0700

    menambahkan kode js untuk mengatur elemen pada file assets/js/swiper-init.js

---
+++ assets/js/swiper-init.js
@@ -0,0 +1,48 @@
+// swiper halaman rooms user
+
+document.addEventListener('DOMContentLoaded', function () {
+    const galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+  
+    const galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    });
+  });
+
+  // swiper halaman dashboard_user
+
+  document.addEventListener("DOMContentLoaded", function () {
+    new Swiper("#swiper-3d-coverflow-effect", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  });
+
+
+
+
+  

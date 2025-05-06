c9e31c918e52f8202a183ba21e674741e63543af
farra-fawnia <farrafawnia01@gmail.com>
2025-05-07 06:07:25 +0700

    memperbarui tampilan navbar_user.php

--- .includes/navbar_user.php
+++ .includes/navbar_user.php
@@ -1,6 +1,6 @@
 <!-- Navbar -->
-<nav class="layout-navbar container-xxl navbar navbar-expand-xl
-navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
+<nav class="layout-navbar navbar navbar-expand-xl
+ align-items-center" id="layout-navbar">
   
   <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
   <nav class="navbar navbar-example navbar-expand-lg">
@@ -25,7 +25,7 @@ navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
 
     <!-- notif -->
     <a href="rincian_pesanan.php">
-    <button type="button" class=" nav-item nav-link btn rounded-pill btn-primary d-flex justify-content-center align-items-center p-0" style="width: 40px; height: 40px;"> 
+    <button type="button" class=" nav-item nav-link btn rounded-pill btn-warning d-flex justify-content-center align-items-center p-0 me-3" style="width: 40px; height: 40px;"> 
   <span class="icon-base bx bx-bell icon-sm"></span> 
 </button>
 </a>
@@ -33,37 +33,12 @@ navbar-detached align-items-center bg-navbar-theme" id="layout-navbar">
 
       <!-- User -->
       <li class="nav-item navbar-dropdown dropdown-user dropdown">
-        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-          <div class="avatar avatar-online">
-            <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-          </div>
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end">
           <li>
-            <a class="dropdown-item" href="#">
-              <div class="d-flex">
-                <div class="flex-shrink-0 me-3">
-                  <div class="avatar avatar-online">
-                    <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-                  </div>
-                </div>
-                <div class="flex-grow-1">
-                  <span class="fw-semibold d-block"><?php echo $name; ?></span>
-                  <small class="text-muted"><?php echo $role; ?></small>
-                </div>
-              </div>
-            </a>
+          <a href="auth/logout.php" class="btn btn-warning mb-1 me-4"> 
+          <i class="bx bx-power-off me-2"></i>
+          <span class="align-middle">Log Out</span>
+          </a>
           </li>
-          <li>
-            <div class="dropdown-divider"></div>
-          </li>
-          <li>
-            <a class="dropdown-item" href="auth/logout.php">
-              <i class="bx bx-power-off me-2"></i>
-              <span class="align-middle">Log Out</span>
-            </a>
-          </li>
-        </ul>
       </li>
       <!--/ User -->
     </ul>

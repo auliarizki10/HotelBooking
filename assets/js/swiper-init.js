5506ffcc006a35b51381de532f70913c461e8224
farra-fawnia <farrafawnia01@gmail.com>
2025-05-08 15:22:21 +0700

    menambah kode js pada halaman contact.php pada file assets/js/swiper-init.js

--- assets/js/swiper-init.js
+++ assets/js/swiper-init.js
@@ -1,48 +1,31 @@
 // swiper halaman rooms user
-
 document.addEventListener('DOMContentLoaded', function () {
-    const galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      slidesPerView: 4,
-      freeMode: true,
-      watchSlidesProgress: true,
-    });
-  
-    const galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
-        swiper: galleryThumbs,
-      },
-    });
+  // Inisialisasi Swiper thumbnails
+  const galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
   });
 
-  // swiper halaman dashboard_user
-
-  document.addEventListener("DOMContentLoaded", function () {
-    new Swiper("#swiper-3d-coverflow-effect", {
-      effect: "coverflow",
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: "auto",
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+  // Inisialisasi Swiper utama
+  const galleryTop = new Swiper('.gallery-top', {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    thumbs: {
+      swiper: galleryThumbs,
+    },
   });
 
-
-
-
-  
+  // Inisialisasi Stepper pada halaman contact user
+  const stepperElement = document.querySelector('.bs-stepper');
+  if (stepperElement) {
+    const stepper = new Stepper(stepperElement, {
+      linear: false,
+      animation: true
+    });
+  }
+});

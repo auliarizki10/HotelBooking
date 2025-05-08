137fac45ad04e0bea868259e47c38082c96d9df2
farra-fawnia <farrafawnia01@gmail.com>
2025-05-08 15:49:42 +0700

    memperbarui tampilan akun user dan mengubah logo pada file .includes/navbar_user.php

--- .includes/navbar_user.php
+++ .includes/navbar_user.php
@@ -8,7 +8,7 @@
                   <nav class="navbar bg-body-tertiary">
                   <div class="container-fluid">
                     <a class="navbar-brand fw-bold d-flex align-items-center fs-3" href="#">
-                    <img src="assets/img/favicon/favicon.ico" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">HOOKING</a>
+                    <img src="assets/img/favicon/hicon.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top">HOOKING</a>
                   </div>
                   </nav>
                     
@@ -25,21 +25,55 @@
 
     <!-- notif -->
     <a href="rincian_pesanan.php">
-    <button type="button" class=" nav-item nav-link btn rounded-pill btn-warning d-flex justify-content-center align-items-center p-0 me-3" style="width: 40px; height: 40px;"> 
+    <button type="button" class=" nav-item nav-link btn rounded-pill btn-warning d-flex justify-content-center align-items-center p-0 me-1" style="width: 40px; height: 40px;"> 
   <span class="icon-base bx bx-bell icon-sm"></span> 
 </button>
 </a>
 <!-- /notif -->
 
       <!-- User -->
-      <li class="nav-item navbar-dropdown dropdown-user dropdown">
+      <!-- <li class="nav-item navbar-dropdown dropdown-user dropdown">
           <li>
           <a href="auth/logout.php" class="btn btn-warning mb-1 me-4"> 
           <i class="bx bx-power-off me-2"></i>
           <span class="align-middle">Log Out</span>
           </a>
           </li>
-      </li>
+      </li> -->
+
+      <li class="nav-item navbar-dropdown dropdown-user dropdown mb-1 me-4">
+        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+          <div class="avatar avatar-online">
+            <img src="assets/img/avatars/pp_user.jpeg" alt class="w-px-40 h-auto rounded-circle" />
+          </div>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end">
+          <li>
+            <a class="dropdown-item" href="#">
+              <div class="d-flex">
+                <div class="flex-shrink-0 me-3">
+                  <div class="avatar avatar-online">
+                    <img src="assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
+                  </div>
+                </div>
+                <div class="flex-grow-1">
+                  <span class="fw-semibold d-block"><?php echo $name; ?></span>
+                  <small class="text-muted"><?php echo $role; ?></small>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <div class="dropdown-divider"></div>
+          </li>
+          <li>
+            <a class="dropdown-item" href="auth/logout.php">
+              <i class="bx bx-power-off me-2"></i>
+              <span class="align-middle">Log Out</span>
+            </a>
+          </li>
+        </ul>
+      </l
       <!--/ User -->
     </ul>
   </div>

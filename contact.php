390cc63c6fab5045d027c598d07e1f28eb9d3cf7
farra-fawnia <farrafawnia01@gmail.com>
2025-05-24 21:29:28 +0700

    menambah komen pada file contact.php

--- contact.php
+++ contact.php
@@ -5,9 +5,9 @@ include (".includes/header_user.php");
 <div class="">
 <div class="col-md pt-4 px-4">
       <h1 class="fw-medium">Customer Service</h1>
-
       <div class="bs-stepper wizard-numbered mt-2 border-bottom">
         <div class="bs-stepper-header">
+          <!-- Tombol Kirimi Kami Pesan -->
           <div class="step active" data-target="#account-details">
             <button type="button" class="step-trigger" data-bs-toggle="step" aria-selected="true">
               <span class="bs-stepper-circle"><i class='bx bx-message-rounded-dots'></i></span>
@@ -17,9 +17,15 @@ include (".includes/header_user.php");
               </span>
             </button>
           </div>
+          <!-- /Tombol Kirimi Kami Pesan -->
+
+           <!-- Panah penghubung antar langkah -->
           <div class="">
             <i class="icon-base bx bx-chevron-right icon-md"></i>
           </div>
+           <!-- /Panah penghubung antar langkah -->
+
+           <!-- Hubungi kami -->
           <div class="step" data-target="#personal-info">
           <button type="button" class="step-trigger" data-bs-toggle="step" aria-selected="true">
               <span class="bs-stepper-circle"><i class='bx bx-phone'></i></span>
@@ -29,9 +35,15 @@ include (".includes/header_user.php");
               </span>
             </button>
           </div>
+          <!-- /Hubungi kami -->
+
+          <!-- Panah penghubung antar langkah (2)-->
           <div class="">
             <i class="icon-base bx bx-chevron-right icon-md"></i>
           </div>
+          <!-- /Panah penghubung antar langkah (2)-->
+
+          <!-- Hubungi Akomodasi -->
           <div class="step" data-target="#social-links">
           <button type="button" class="step-trigger" data-bs-toggle="step" aria-selected="true">
               <span class="bs-stepper-circle"><i class='bx bx-building-house'></i></span>
@@ -41,9 +53,12 @@ include (".includes/header_user.php");
               </span>
             </button>
           </div>
+          <!-- /Hubungi Akomodasi -->
         </div>
+
+        <!-- BAGIAN KETERANGAN SINGKAT -->
         <div class="bs-stepper-content">
-            <!-- Account Details -->
+            <!-- Keterangan singkat: kirimi kami pesan -->
             <div id="account-details" class="content active dstepper-block">
               <div class="row g-6">
               <div class="p-2">
@@ -53,7 +68,9 @@ include (".includes/header_user.php");
       </div>
               </div>
             </div>
-            <!-- Personal Info -->
+            <!-- /Keterangan singkat: kirimi kami pesan -->
+
+            <!-- Keterangan singkat: hubungi kami -->
             <div id="personal-info" class="content">
               <div class="row g-6">
                 <div class="col-sm-6">
@@ -65,27 +82,30 @@ include (".includes/header_user.php");
               </div>
             </div>
         </div>
-        <!-- Social Links -->
+        <!-- /Keterangan singkat: hubungi kami -->
+
+        <!-- Keterangan singkat: hubungi akomodasi -->
         <div id="social-links" class="content">
-              <div class="row g-6">
-              <div class="col-sm-6">
+          <div class="row g-6">
+            <div class="col-sm-6">
               <div class="p-2">
-        <h6 class="mb-0" style="font-family: 'Poppins', sans-serif; line-height: 1.6;">
-        Hubungi kami untuk detail lengkap mengenai akomodasi dan layanan yang tersedia.
-        </h6>
-      </div>
-      </div>
+                <h6 class="mb-0" style="font-family: 'Poppins', sans-serif; line-height: 1.6;">
+                Hubungi kami untuk detail lengkap mengenai akomodasi dan layanan yang tersedia.
+                </h6>
+                  </div>
+                </div>
               </div>
             </div>
+            <!-- /Keterangan singkat: hubungi akomodasi -->
       </div>
+      <!--/ BAGIAN KETERANGAN SINGKAT -->
       </div>
 
-
+      <!-- BAGIAN PERTANYAAN UMUM -->
       <h3 class="">Pertanyaan umum</h3>
       <div class="accordion mt-4" id="accordionStyle1">
         <!-- pertanyaan 1 -->
   <div class="accordion-item border">
-    <!-- flex container untuk ikon + button -->
     <div class="accordion-header mb-0">
       <button type="button" class="accordion-button collapsed text-dark fw-bold py-1 d-flex align-items-center gap-2 px-3 py-2 border border-dark"
               data-bs-toggle="collapse" data-bs-target="#accordionStyle1-1" aria-expanded="false">
@@ -94,6 +114,7 @@ include (".includes/header_user.php");
               style="width: 30px; height: 30px;">
           <i class="bx bx-help-circle fs-2 text-dark"></i>
         </span>
+        
         <!-- teks header -->
          <span>Bagaimana cara mengakses pemesanan online saya?</span>
       </button>
@@ -108,7 +129,6 @@ include (".includes/header_user.php");
 
   <!-- pertanyaan 2 -->
   <div class="accordion-item">
-    <!-- flex container untuk ikon + button -->
     <div class="accordion-header mb-0">
       <button type="button" class="accordion-button collapsed text-dark fw-bold py-1 d-flex align-items-center gap-2 px-3 py-2 border border-dark"
               data-bs-toggle="collapse" data-bs-target="#accordionStyle1-2" aria-expanded="false">
@@ -131,7 +151,6 @@ include (".includes/header_user.php");
 
   <!-- pertanyaan 3 -->
   <div class="accordion-item border mb-2">
-        <!-- flex container untuk ikon + button -->
         <div class="accordion-header mb-0">
           <button type="button" class="accordion-button collapsed text-dark fw-bold py-1 d-flex align-items-center gap-2 px-3 py-2 border border-dark"
                   data-bs-toggle="collapse" data-bs-target="#accordionStyle1-3" aria-expanded="false">
@@ -152,9 +171,10 @@ include (".includes/header_user.php");
     </div>
   </div>
     </div>
+    <!-- /BAGIAN PERTANYAAN UMUM -->
     </div>
     </div>
 
-    <?php
+<?php
 include (".includes/footer_user.php");
 ?>
